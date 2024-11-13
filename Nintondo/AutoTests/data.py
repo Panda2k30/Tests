8844648b2add226b1f532b097851dccb6c39b5cf
Alexsey <pandashow2k30@gmail.com>
2024-11-13 18:03:51 +0300

    update

--- Nintondo/AutoTests/data.py
+++ Nintondo/AutoTests/data.py
@@ -1,24 +1,15 @@
 class Data:
 
-    # Данные для реги
-    # WALLET_PASSWORD = "ThisIsVal1dPassw0rd"
-    WALLET_CONFPASSWORD = "ThisIsVal1dPassw0rd"
-    
-    WALLET_NEWPASSWORD = "Nint0ndoWa11et"
-    WALLET_NEWPASSWORD_CONF = "Nint0ndoWa11et"
-
-    NOTVALID_PASS = ""
-    NOTVALID_CONFPASS = ""
-
-    # Тестовый приватник
+    # Test private keys
     KEY_MONEY_WALLET = "PihWthEm68TGZ2iWoDRWYJLmXPr5itHvKnhmYBz6vseVV6dN1rdU"
     INVALID_KEY_WALLET = "PihWthEm68TGZ2iWoDRWYJLmXPr5itHvKnhmYBz6vseVV6dN1rd"
 
     KEY_WALLET_FOR_CHECK = "PkaP9YReRMtu42cXvCUdBYJmXFqTNY3Cjd9d9bM4qskMt6xbQcCv"
 
+    # Test mnemonics
     VALID_MNEMONIC_DATA = ["father", "wing", "predict", "type", "click", "sunset", "legal", "garden", "current", "blood", "mushroom", "baby"]
     INVALID_MNEMONIC_DATA = ["father", "wing", "predict", "type", "click", "sunset", "", "garden", "current", "blood", "mushroom", "baby"]
 
-    # Адреса кошельков
+    # Wallet addresses
     VALID_ADDRESS_FOR_CHECK = "tbel1q0v7cjajnlkll0ppjsmdduwh2gfjml3nhmuks24"
     NOT_VALID_ADDRESS = "1tsn0tval1dAddre5"
